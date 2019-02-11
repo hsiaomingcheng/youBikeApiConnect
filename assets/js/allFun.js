4ba29b68a4714cd5dc73f7d451d111937d2c3819
chris_home <hsiao516@gmail.com>
2019-02-12 00:28:35 +0800

    把CDN改成內部引入Vue 然後多加了devtool幫助開發檢測 最後把Ajax的功能移到methods裡面放

--- assets/js/allFun.js
+++ assets/js/allFun.js
@@ -1,5 +1,5 @@
 //所有功能JS的家
-
+Vue.config.devtools = true; //这步很重要
 
 var startVue = new Vue({
     el: '#app',
@@ -13,7 +13,36 @@ var startVue = new Vue({
         next: 'next',
     },
     methods: {
-        changeDataEvent(){
+        callAjax: function(){
+            var obj = '';
+            var request = new XMLHttpRequest();
+
+            request.onload = function() {
+
+                //回應狀態碼判斷  判斷是 2xx 的成功、3xx 的重新導向 或 4xx 客戶端錯誤
+                if (request.status >= 200 && request.status < 400) {
+
+                    // Success!
+                    // console.log(request.responseText);
+
+                    //這裡的 this 等於 XMLHttpRequest
+                    //console.log(this); == XMLHttpRequest
+
+                    //把對方回傳的資料轉成JSON格式
+                    obj = JSON.parse(request.responseText);
+
+                    startVue.bikeDataArray = obj.result.results.slice(0, 10); //成功讀取後 先秀10筆資料
+                    startVue.count    = 0; //指定初始count為0
+                    startVue.maxCount = Math.floor( obj.result.count / 10 );
+
+                    startVue.dataFile = obj.result.results; //將取得的資料放入陣列
+                }
+            };
+
+            request.open('GET', 'https://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=ddb80380-f1b3-4f8e-8016-7ed9cba571d5', true);
+            request.send();
+        },
+        changeDataEvent: function(){
             //動態切換事件
             // this.bikeDataArray = this.dataFile.slice(10, 20);
 
@@ -32,32 +61,7 @@ var startVue = new Vue({
         }
     },
     mounted(){
-        var obj = '';
-        var request = new XMLHttpRequest();
-
-        request.onload = function() {
-
-            //回應狀態碼判斷  判斷是 2xx 的成功、3xx 的重新導向 或 4xx 客戶端錯誤
-            if (request.status >= 200 && request.status < 400) {
-
-                // Success!
-                // console.log(request.responseText);
-
-                //把對方回傳的資料轉成JSON格式
-                obj = JSON.parse(request.responseText);
-
-                startVue.bikeDataArray = obj.result.results.slice(0, 10); //成功讀取後 先秀10筆資料
-                startVue.count    = 0; //指定初始count為0
-                startVue.maxCount = Math.floor( obj.result.count / 10 );
-
-                startVue.dataFile = obj.result.results; //將取得的資料放入陣列
-            }
-            console.log(obj);
-        };
-
-        // request.open('GET', 'http://e-traffic.taichung.gov.tw/DataAPI/api/YoubikeAllAPI', true);
-        request.open('GET', 'https://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=ddb80380-f1b3-4f8e-8016-7ed9cba571d5', true);
-        request.send();
+        this.callAjax();
     }
 });
 
