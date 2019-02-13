63a04f7e7c1501d0f0551b428330ddbab89b8ae2
chris_home <hsiao516@gmail.com>
2019-02-14 00:41:52 +0800

    新增自動更新內容功能 100秒會重抓取一次資料

--- assets/js/allFun.js
+++ assets/js/allFun.js
@@ -1,12 +1,31 @@
 //所有功能JS的家
 Vue.config.devtools = true; //这步很重要
 
+
+/*
+台北市Youbike API
+sno：站點代號、
+sna：場站名稱(中文)、
+tot：場站總停車格、
+sbi：場站目前車輛數量、
+sarea：場站區域(中文)、
+mday：資料更新時間、
+lat：緯度、
+lng：經度、
+ar：地(中文)、
+sareaen：場站區域(英文)、
+snaen：場站名稱(英文)、
+aren：地址(英文)、
+bemp：空位數量、
+act：全站禁用狀態
+*/
+
 var startVue = new Vue({
     el: '#app',
     data: {
         bikeDataArray: [],  //顯示的陣列
         dataFile: [],       //放所有筆數的陣列
-        count: '',          //現在數
+        count: 0,           //現在數
         maxCount: '',       //最大數
         showNum: 10,        //想顯示的筆數(預設10筆),
         numInput: '',
@@ -18,8 +37,7 @@ var startVue = new Vue({
             var obj = '';
             var request = new XMLHttpRequest();
 
-            request.onload = function() {
-
+            request.addEventListener("load", function(){
                 //回應狀態碼判斷  判斷是 2xx 的成功、3xx 的重新導向 或 4xx 客戶端錯誤
                 if (request.status >= 200 && request.status < 400) {
 
@@ -32,16 +50,25 @@ var startVue = new Vue({
                     //把對方回傳的資料轉成JSON格式
                     obj = JSON.parse(request.responseText);
 
-                    startVue.bikeDataArray = obj.result.results.slice(0, 10); //成功讀取後 先秀10筆資料
-                    startVue.count    = 0; //指定初始count為0
-                    startVue.maxCount = Math.floor( obj.result.count / 10 );
-
                     startVue.dataFile = obj.result.results; //將取得的資料放入陣列
-                }
-            };
 
-            request.open('GET', 'https://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=ddb80380-f1b3-4f8e-8016-7ed9cba571d5', true);
+                    if ( startVue.count == 0 ) {
+                        startVue.bikeDataArray = obj.result.results.slice(0, 10); //成功讀取後 先秀10筆資料
+                        startVue.maxCount = Math.floor( obj.result.count / 10 );
+                    }
+                }
+            });
+
+            request.open('GET', 'http://data.ntpc.gov.tw/api/v1/rest/datastore/382000000A-000352-001', true);
             request.send();
+        },
+        timerAjax: function(){
+
+            var _this = this;
+
+            setInterval(function(){
+                _this.callAjax();
+            }, 100000);
         },
         changeShowNum: function(){
             
@@ -75,6 +102,8 @@ var startVue = new Vue({
     },
     mounted(){
         this.callAjax();
+
+        this.timerAjax();
     }
 });
 
