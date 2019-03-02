290b40dd439581641e2bc84557eac519dc248bec
chris_home <hsiao516@gmail.com>
2019-03-03 00:14:56 +0800

    下拉篩選區域功能

--- assets/js/allFun.js
+++ assets/js/allFun.js
@@ -21,36 +21,45 @@ act(場站是否暫停營運)
 */
 
 
+
 var startVue = new Vue({
     el: '#app',
     data: {
         bikeDataArray: [],  //顯示的陣列
         dataFile: [],       //放所有筆數的陣列
+        dataFileBox: [],    //所有筆數的儲藏處
         count: 0,           //現在數
         maxCount: '',       //最大數
         showNum: 10,        //想顯示的筆數(預設10筆),
         numInput: '',
         prev: 'prev',
         next: 'next',
+        area: [
+            '全區域','板橋區','三重區','中和區','新莊區','永和區',
+            '新店區','土城區','蘆洲區','樹林區','汐止區','三峽區',
+            '淡水區','鶯歌區','五股區','泰山區','林口區','瑞芳區',
+            '深坑區','石碇區','坪林區','三芝區','石門區','八里區',
+            '平溪區','雙溪區','貢寮區','金山區','萬里區','烏來區',
+        ]
     },
     methods: {
         callAjax: function(){
             var obj = '';
 
             axios
-            .get('https://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=ddb80380-f1b3-4f8e-8016-7ed9cba571d5')
+            .get('https://script.google.com/macros/s/AKfycbxs3QX-GGwWZiGEloxDiuM81fu6IXidHesQEZaGinQ7YEbMmf0/exec?url=data.ntpc.gov.tw/api/v1/rest/datastore/382000000A-000352-001')
             .then(function (response) {
                 // handle success
-                console.log(response);
+                // console.log(response);
 
-                
                 obj = response.data;
 
-                startVue.dataFile = obj.result.results;
+                startVue.dataFileBox = obj.result.records;
+                startVue.dataFile    = startVue.dataFileBox;
 
                 if ( startVue.count == 0 ) {
-                    startVue.bikeDataArray = startVue.dataFile.slice(0, 10);
-                    startVue.maxCount = Math.floor( obj.result.count / 10 );
+                    startVue.bikeDataArray = startVue.dataFileBox.slice(0, 10);
+                    startVue.maxCount = Math.floor( obj.result.total / 10 );
                 }
 
             });
@@ -91,6 +100,27 @@ var startVue = new Vue({
                 foot = (this.count * this.showNum) + this.showNum;
 
             this.bikeDataArray = this.dataFile.slice(head, foot);
+        },
+        selectDataEvent: function(){
+            var tempArray = [],
+                sarea     = '',
+                value     = this.$refs.selectValue.value;
+
+            if ( value == '全區域' ) {
+                this.dataFile = this.dataFileBox;
+                this.changeDataEvent();
+            }else {
+                this.dataFileBox.forEach(function(currentValue, index, arr){
+                    sarea = currentValue.sarea;
+    
+                    if ( sarea == value ) {
+                        tempArray.push(arr[index]);
+                    }
+                });
+    
+                this.dataFile = tempArray;
+                this.changeShowNum();
+            }
         }
     },
     mounted(){
